e2e688711a92bbaab963c583c1fbbcc3f5fd5a5d
정다은 <wjdekdms001@gmail.com>
2023-01-27 00:23:28 +0900

    100/100

--- 2023KAKAO/4_binarytree.py
+++ 2023KAKAO/4_binarytree.py
@@ -1,62 +1,44 @@
 import math
 
 def dfs (temp:list, i:int, depth):
-    if depth < 1 :
+    if depth == 0 :
         return 1
 
-    l = int(i - 2 ** (depth-1))
-    r = int(i + 2 ** (depth-1))
-
-    if temp[i-1] == '0':
-        if temp[l-1] == '1' or temp[r-1] == '1':
+    if temp[i] == '0':
+        if temp[i - depth] == '1' or temp[i + depth] == '1':
             return 0
 
-    return dfs(temp, l, depth - 1) * dfs(temp, r, depth - 1)
+    print(i - depth, i, i + depth)
+
+    l = dfs(temp, i - depth, depth // 2)
+    r = dfs(temp, i + depth, depth // 2)
+
+    return l * r
 
 def solution(numbers):
     answer = []
     for item in numbers:
-        temp = list(bin(item)[2:])
-        depth = math.ceil(math.log2(len(temp)))
+        temp = bin(item)[2:]
+        d = len(bin(len(temp))[2:]) # math 사용하지 않고 len과 bin 활용하는게 핵심!
 
-        while len(temp) < 2 ** depth - 1:
-            temp.insert(0, '0')
+        while len(temp) < 2 ** d:
+            temp = '0' + temp
 
-        answer.append(dfs(temp, (len(temp)+1) // 2, depth - 1))
+        i = len(temp) // 2
+        # print(c)
+
+        print(d, temp, i, i // 2)
+        answer.append(dfs(temp, i, i//2))
+        del temp
     return answer
 
 
-# 예외처리가 틀린건 없음
-# 나중에 오답하고 우선 dfs 로 방식 변경해서 구현 해도 똑같다.
-#
-# math 가 문제일까?
 
-
+# print(bin(7) + bin(1))
 
 #
-# def solution(numbers):
-#     answer = []
-#     for item in numbers:
-#         temp = list(bin(item)[2:])
-#         depth = math.ceil(math.log2(len(temp)))
-#         while len(temp) < (2 ** depth - 1):
-#             temp.insert(0, '0')
-#         check = 0
-#         for k in range(1, depth):
-#             for i in range((len(temp)+1) // 2 ** (k+1)):
-#                 mid = 2 **k + 2 ** (k+1) * i
-#                 if temp[mid-1] == '0':
-#                     if temp[mid - 2 **(k-1) - 1] == '1' or temp[mid + 2 ** (k - 1) - 1] == '1':
-#                         check += 1
-#         del temp
-#         if check > 0:
-#             answer.append(0)
-#         else:
-#             answer.append(1)
-#     return answer
-
 print(solution([7, 42, 5]))
-print(solution([63, 111, 95, 165150075007]))
+# print(solution([63, 111, 95, 165150075007]))
 
 
 # 합계: 10.0 / 100.0 맞는거 같은데 ㅡㅡ 화가난다.. 일단 오늘은 여기까지
