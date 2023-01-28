3849d79149a3b94a44d44769a268fded38f8c5a5
GraceCheong <wjdekdms001@gmail.com>
2023-01-28 12:47:07 +0900

    maze 16.3 / 100.0

--- 2023KAKAO/6_maze.py
+++ 2023KAKAO/6_maze.py
@@ -4,16 +4,12 @@ def find_road(n, m, Nx, Ny, Ex, Ey, k, count, li:list, way:str):
             li.append(way)
         return
     else :
-        #able to go up
         if Nx > 1 :
             find_road(n, m, Nx-1, Ny, Ex, Ey, k, count+1, li, way+"u")
-        #able to go down
         if Nx < n :
             find_road(n, m, Nx+1, Ny, Ex, Ey, k, count+1, li, way+"d")
-        #able to go right
         if Ny < m :
             find_road(n, m, Nx, Ny+1, Ex, Ey, k, count+1, li, way+"r")
-        #able to go left
         if Ny > 1 :
             find_road(n, m, Nx, Ny-1, Ex, Ey, k, count+1, li, way+"l")
         return
@@ -21,9 +17,7 @@ def find_road(n, m, Nx, Ny, Ex, Ey, k, count, li:list, way:str):
 def solution(n, m, x, y, r, c, k):
     li = []
     answer = ''
-
     find_road(n, m, x, y, r, c, k, 0, li, "")
-
     if len(li) > 0:
         _min = "z"
         for item in li :
@@ -32,5 +26,4 @@ def solution(n, m, x, y, r, c, k):
         answer = _min
     else :
         answer = "impossible"
-
     return answer
