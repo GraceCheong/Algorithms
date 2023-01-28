39203e1f275d98abbe4b45d4251842ebfde0ee6c
GraceCheong <wjdekdms001@gmail.com>
2023-01-28 12:39:08 +0900

    maze 16.3 / 100.0

--- 2023KAKAO/6_maze.py
+++ 2023KAKAO/6_maze.py
@@ -16,13 +16,16 @@ def find_road(n, m, Nx, Ny, Ex, Ey, k, count, li:list, way:str):
         #able to go left
         if Ny > 1 :
             find_road(n, m, Nx, Ny-1, Ex, Ey, k, count+1, li, way+"l")
-
+        return
 
 def solution(n, m, x, y, r, c, k):
     li = []
+    answer = ''
+
     find_road(n, m, x, y, r, c, k, 0, li, "")
+
     if len(li) > 0:
-        _min = "z"*2500
+        _min = "z"
         for item in li :
             if _min > item:
                 _min = item
