0df69724298347b7e8d27dfddab2e87078352ca6
GraceCheong <wjdekdms001@gmail.com>
2023-01-28 13:04:58 +0900

    maze 16.3 / 100.0

--- 2023KAKAO/6_maze.py
+++ 2023KAKAO/6_maze.py
@@ -1,29 +1,35 @@
-def find_road(n, m, Nx, Ny, Ex, Ey, k, count, li:list, way:str):
-    if count == k :
-        if Nx == Ex and Ny == Ey :
-            li.append(way)
-        return
-    else :
-        if Nx > 1 :
-            find_road(n, m, Nx-1, Ny, Ex, Ey, k, count+1, li, way+"u")
-        if Nx < n :
-            find_road(n, m, Nx+1, Ny, Ex, Ey, k, count+1, li, way+"d")
-        if Ny < m :
-            find_road(n, m, Nx, Ny+1, Ex, Ey, k, count+1, li, way+"r")
-        if Ny > 1 :
-            find_road(n, m, Nx, Ny-1, Ex, Ey, k, count+1, li, way+"l")
-        return
-
 def solution(n, m, x, y, r, c, k):
     li = []
     answer = ''
-    find_road(n, m, x, y, r, c, k, 0, li, "")
-    if len(li) > 0:
-        _min = "z"
-        for item in li :
-            if _min > item:
-                _min = item
-        answer = _min
+
+    # 프로그래머스 김도원님 코드 참고
+    diff = abs(r-x) + abs(c-y)
+    # 예외처리 먼저 : 출발지점과 도착지점 사이의 칸수 차이가 움직임 횟수보다 크거나 홀짝수가 안맞으면 pass
+    if diff%2!=k%2 or diff>k:
+        return 'impossible'
+
+    #dlru 순서로 가기
+    #왜냐면 어차피 맨 앞에꺼만 구하면되기 때문에
+    dcount = 0
+    lcount = 0
+    rcount = 0
+    ucount = 0
+
+    if x < r :
+        dcount = r-x
     else :
-        answer = "impossible"
+        ucount = x-r
+    if c < y :
+        lcount = y-c
+    else :
+        rcount = c-y
+
+    rest = k - diff
+
+    xplus = min(n-max(x,r), rest//2)
+    rest -= xplus * 2
+    yplus = min(min(y, c)-1, rest//2)
+    rest -= yplus*2
+
+    answer = 'd'*(dcount+xplus)+'l'*(lcount+yplus)+'rl'*(rest//2)+'r'*(rcount+yplus)+'u'*(xplus+ucount)
     return answer
