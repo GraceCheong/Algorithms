020b8c04e421e747e080f7e411819d292aec60d3
GraceCheong <wjdekdms001@gmail.com>
2023-02-02 00:56:59 +0900

    짜증 왜 시간초과?

--- unsolved/BOJ12865.py
+++ unsolved/BOJ12865.py
@@ -2,26 +2,22 @@
 Problem: 평범한 배낭(12865)
 BaekJoon Online
 '''
-def put(k, max_val, sum_val, sum_weight, li:list, index, depth):
-    depth += 1
-    if sum_weight > k or index > len(li)-1 : 
-        return max_val
-    print(depth)
-    nw, nv = li[index] # now value
-    
-    _max_val = max_val
-    
-    if sum_weight + nw <= k : 
-        _max_val = max(_max_val, sum_val + nv, put(k, _max_val, sum_val + nv, sum_weight + nw, li, index+1, depth))
+def put(k, stack: list):
+    if len(stack) <= 0 or k <= 0: 
+        return 0
+    s = stack.copy()
 
-    for i in range(len(li)):
-        _max_val = max(_max_val, 
-                    put(k, _max_val, sum_val, sum_weight, li, index + i + 1, depth), # 없는 애들중 최고 
-                    ) 
-
-    return _max_val
+    w, v = s.pop()
+    if not s :
+        if w > k : 
+            return 0 
+    
+    res = max(put(k, s), (v + put(k-w, s)) if k-w >=0 else 0)
+    #print(k, w, v, s, res)
+    del stack 
+    return res 
 
 n, k = map(int, input().split())
 stufflist = [list(map(int, input().split())) for i in range(n)]
 
-print(put(k, 0, 0, 0, stufflist, 0, 0))
+print(put(k, stufflist))
