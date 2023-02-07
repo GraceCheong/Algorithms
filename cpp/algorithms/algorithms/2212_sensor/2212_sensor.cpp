aa41c16be17bb74ea051a7216cc7e9506dd21608
GraceCheong <wjdekdms001@gmail.com>
2023-02-08 07:59:52 +0900

    not getting the answer

--- cpp/algorithms/algorithms/2212_sensor/2212_sensor.cpp
+++ cpp/algorithms/algorithms/2212_sensor/2212_sensor.cpp
@@ -4,6 +4,17 @@
 //
 //  Created by 정다은 on 2023/02/08.
 //
-
+#include <iostream>
 #include <stdio.h>
+
+
+using namespace std;
+
+int main()
+{
+    int N, K, data, max = INT_MIN, min = INT_MAX;
+    cin >> N >> K;
+    
+    
+}
 
