09eadfd9944d3980df50e51527d7ac5b0a8cb6fb
GraceCheong <wjdekdms001@gmail.com>
2023-02-08 09:05:08 +0900

    Update 2212_sensor.cpp

--- cpp/algorithms/algorithms/2212_sensor/2212_sensor.cpp
+++ cpp/algorithms/algorithms/2212_sensor/2212_sensor.cpp
@@ -6,15 +6,29 @@
 //
 #include <iostream>
 #include <stdio.h>
-
+#include <list>
 
 using namespace std;
 
 int main()
 {
-    int N, K, data, max = INT_MIN, min = INT_MAX;
+    int N, K, data;
     cin >> N >> K;
-    
+
+    list<int> sensors = new list<int>(); 
+    list<int>::iterator it = sensors.begin();
+
+    for (int i = 0; i < N; i ++)
+    {
+        cin >> data; 
+        sensors.push_back(data);
+    }
+
+    sensors.sort();
+    for (it = sensors.begin(); it != sensors.end(); ++it)
+    {
+        
+    }
     
 }
 
